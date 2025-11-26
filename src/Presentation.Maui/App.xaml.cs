260fb126d7d797bf2aef130477b86c67adaf58c3
https://hll2975.github.io/M-Hoja-de-Vida-/Hernán-Luis-IT <56049876+hernan2975@users.noreply.github.com>
2025-11-26 14:39:10 -0300

    Create App.xaml.cs

---
+++ src/Presentation.Maui/App.xaml.cs
@@ -0,0 +1,10 @@
+namespace PampaGanadero.Presentation.Maui;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+        MainPage = new MainPage();
+    }
+}
